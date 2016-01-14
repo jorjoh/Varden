64acd608dcebacf2eef266c6c500540368e03048
Erik Røed <post@erikroed.no>
2016-01-14 12:05:47 +0100

    Lagt til SVG for å teste bruk av SVG

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Varden.no - Online Bildearkiv</title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
+            <p>Her kommer online bildearkiv til Varden</p>
+        </body>
+    </html>
