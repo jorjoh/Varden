b4df9a8d0a4b13fe34f44e2d16320e4c6df0ce5a
Erik Røed <post@erikroed.no>
2016-01-20 00:46:37 +0100

    Bygd opp responsiv grid for frontend med css og eksempler p8;103R bruk av grid i HTML

--- index.php
+++ index.php
@@ -2,13 +2,58 @@
     <html>
         <head>
             <title>Varden.no - Online Bildearkiv</title>
+            <meta charset="UTF-8">
             <link rel="stylesheet" type="text/css" href="style/style.css">
         </head>
         <body>
-            <a href="administrator/index.php">Administrator</a>
-            <p>Her kommer online bildearkiv for varden</p>
+            <div class="row">
+                <div class="col-12">
+                    <h1 style="text-align: center;">Lørdagsmagasinet</h1>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut rutrum magna. Suspendisse potenti. Etiam eu orci turpis. Ut volutpat tempor quam eget ullamcorper. Mauris elementum ante ut suscipit imperdiet. Suspendisse potenti. Praesent ultricies eget ligula eu aliquam. Sed fermentum dignissim dui vel tristique. Maecenas sagittis diam at sodales dapibus. Fusce in accumsan purus. Nunc at nibh quis metus dignissim interdum vitae vitae nisi.
+
+                    Nullam eleifend lacus et leo interdum, vel sodales nibh rutrum. Integer quam libero, viverra non tellus vel, faucibus rhoncus justo. Aenean id justo mollis, sollicitudin tortor vel, tincidunt orci. Nunc id mi at justo scelerisque cursus a nec diam. Curabitur non mi ut eros rutrum ullamcorper. Cras nec felis at velit consequat commodo gravida vitae purus. Maecenas feugiat risus ac maximus dictum. Ut dictum, elit a lobortis sollicitudin, elit dolor sagittis orci, sit amet sollicitudin leo eros at mauris. Cras scelerisque elit quis augue posuere, at condimentum lectus fringilla. Nullam posuere nunc enim, id aliquet lectus luctus a. Cras rhoncus euismod justo, id facilisis ipsum suscipit at. Morbi sed scelerisque erat, nec fringilla nisl.
+
+                    Nullam tincidunt in nibh quis efficitur. Pellentesque mauris magna, ultrices sit amet varius vel, faucibus ut quam. Vivamus venenatis orci vel ante vulputate, sed condimentum ante aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur elit justo, ultricies in hendrerit vel, volutpat ut elit. Duis elementum eros non ante suscipit interdum. Integer dapibus mi vitae pharetra ornare. Proin non aliquam nisl, eu ultrices nibh.
+
+                    Quisque fermentum mi tellus, ut tincidunt justo consequat ut. Integer sed tempus massa. In vehicula placerat vulputate. Proin sed pellentesque metus. Nulla facilisi. Nullam auctor ligula at ipsum accumsan, eget viverra purus convallis. Quisque imperdiet tempus mauris, ullamcorper pellentesque libero consequat at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse urna nunc, imperdiet a lobortis ut, lacinia et erat. Nunc a tincidunt risus. Etiam vitae justo non erat fermentum convallis eleifend sit amet est. Curabitur aliquet rhoncus sodales. Integer pellentesque ex id rutrum blandit. Donec varius, odio et pulvinar mollis, libero nunc dapibus ligula, sit amet sodales est lorem id enim.
+
+                    Vivamus egestas mauris ac accumsan dignissim. Nulla vel pretium velit, ut hendrerit est. Nam magna mauris, porttitor maximus tincidunt a, venenatis sit amet neque. Nullam tempus sem at eleifend pellentesque. Donec eu sagittis enim, ut venenatis ante. Nullam congue orci enim, at condimentum sem egestas ac. Nulla facilisi. Donec ante erat, faucibus quis nibh et, vehicula ultrices enim. Aenean et lacinia tellus. Donec malesuada consequat lectus, quis blandit turpis iaculis accumsan. Morbi vitae fringilla nisi. Pellentesque quis tortor posuere quam porta elementum a quis sem. Sed scelerisque commodo mi nec lobortis. Proin nibh lectus, convallis sed nisl suscipit, imperdiet venenatis mi.
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <h3>Sidemeny</h3>
+                    <?php
+                        for($i = 0; $i < 11; $i++) {
+                            echo "<li>Menypunkt $i</li>";
+                        }
+                    ?>
+                </div>
+                <div class="col-9">
+                    <h1>Innhold</h1>
+                    <br>
+                    <p>Her kommer masse innhold på siden etterhvert som vi får utviklet den videre!</p>
+                </div>
+            </div>
+            <div class="row">
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-9">
+                    <h1>Innhold</h1>
+                    <p>Her kommer det masse innhold når siden er ferdig. Dette for å vise forskjellige måter man kan bruke grid systemet</p>
+                </div>
+                <div class="col-1">
+                    <p>Skrevet av: Ola Nordmann</p>
+                </div>
+                <div class="col-2">
+                    Spaltehjørnet er i dag skrevet av Ola Vigen Hattestad
+                </div>
+            </div>
         </body>
-        <footer>
-            <p>Footer kommer her</p>
-        </footer>
     </html>
