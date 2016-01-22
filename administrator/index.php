ea473fa06ca35c342b8d2c879ebb5acabf3109fd
Jørgen Johansen <post@jorgenjohansen.no>
2016-01-22 13:41:36 +0100

    Implents menu

--- administrator/index.php
+++ administrator/index.php
@@ -10,31 +10,78 @@
 <!DOCTYPE html>
     <html>
         <head>
-            <title>Online Bildearkiv -admin</title>
+            <title>Online Bildearkiv - adminpanel</title>
          <link rel="stylesheet" type="text/css" href="style/styleBackend.css">
             <link rel="icon" type="image/png" href="../img/VA-fav-icon-152.png">
         </head>
         <body>
             <img src="img/vardenlogo.PNG">
             <div class="row">
-                <nav id="menuHandler">
-                    <ul>
-                        <li><a href="../index.php">Forside</a></li>
-                        <li>
-                            <a href="#">Last opp bilder</a>
-                        </li>
-                        <li><a href="#">Forslag</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="row">
                 <div class="col-12">
                     <h1 class="intro">Velkommen til administreringssiden til Onlinebildearkiv</h1>
-                    <div class="content"></div>
+                    <div class="row">
+                        <div class="col-5">
+                            <nav id="nav">
+                                <li class="current"><a href="../index.php">Hjem</a></li>
+                                <li><a href="#">Last opp bilde</a>
+                                    <ul>
+                                        <li><a href="#">Last opp et bilde</a>
+                                            <ul>
+                                                <li><a href="#">Samling</a></li>
+                                                <li><a href="#">Jørgen</a></li>
+                                                <li><a href="#">Erik</a></li>
+                                                <li><a href="#">Nok en under-element</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Last opp flere bilder</a>
+                                            <ul>
+                                                <li><a href="#">Dette er en my undermeny</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">IconDock</a></li>
+                                        <li><a href="#">Beste Web BildeGalleri</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Historikk</a>
+                                    <ul>
+                                        <li><a href="#">Team</a>
+                                            <ul>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                                <li><a href="#">Sub-Level Item</a>
+                                                    <ul>
+                                                        <li><a href="#">Sub-Level Item</a></li>
+                                                        <li><a href="#">Sub-Level Item</a></li>
+                                                        <li><a href="#">Sub-Level Item</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Historikk bilder</a></li>
+                                        <li><a href="#">Test statestikk</a></li>
+                                        <li><a href="#">Popcorn</a>
+                                            <ul>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Om</a></li>
+                                <li><a href="#">Kontakt oss</a></li>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="content">
+
+                    </div>
                 </div>
+
             </div>
+
         </body>
         <footer>
+
         </footer>
     </html>
