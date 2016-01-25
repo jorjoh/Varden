488c11675d4484f4f0fcad60936f826493f8cf5c
Jørgen <Jrgen>
2016-01-25 14:41:29 +0100

    Fikset litt rundt 1200 gridlayout

--- administrator/index.php
+++ administrator/index.php
@@ -11,7 +11,7 @@
     <html>
         <head>
             <title>Online Bildearkiv - adminpanel</title>
-         <link rel="stylesheet" type="text/css" href="style/styleBackend.css">
+            <link rel="stylesheet" type="text/css" href="style/styleBackend.css">
             <link rel="icon" type="image/png" href="../img/VA-fav-icon-152.png">
         </head>
         <body>
@@ -19,69 +19,71 @@
             <div class="row">
                 <div class="col-12">
                     <h1 class="intro">Velkommen til administreringssiden til Onlinebildearkiv</h1>
-                    <div class="row">
-                        <div class="col-10">
-                            <nav id="nav">
-                                <li class="current"><a href="../index.php">Hjem</a></li>
-                                <li><a href="#">Last opp bilde</a>
-                                    <ul>
-                                        <li><a href="#">Last opp et bilde</a>
-                                            <ul>
-                                                <li><a href="#">Samling</a></li>
-                                                <li><a href="#">Jørgen</a></li>
-                                                <li><a href="#">Erik</a></li>
-                                                <li><a href="#">Nok en under-element</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Last opp flere bilder</a>
-                                            <ul>
-                                                <li><a href="#">Dette er en my undermeny</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">IconDock</a></li>
-                                        <li><a href="#">Beste Web BildeGalleri</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Historikk</a>
-                                    <ul>
-                                        <li><a href="#">Team</a>
-                                            <ul>
-                                                <li><a href="#">Sub-Level Item</a></li>
-                                                <li><a href="#">Sub-Level Item</a>
-                                                    <ul>
-                                                        <li><a href="#">Sub-Level Item</a></li>
-                                                        <li><a href="#">Sub-Level Item</a></li>
-                                                        <li><a href="#">Sub-Level Item</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#">Sub-Level Item</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Historikk bilder</a></li>
-                                        <li><a href="#">Test statestikk</a></li>
-                                        <li><a href="#">Popcorn</a>
-                                            <ul>
-                                                <li><a href="#">Sub-Level Item</a></li>
-                                                <li><a href="#">Sub-Level Item</a></li>
-                                                <li><a href="#">Sub-Level Item</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Om</a></li>
-                                <li><a href="#">Kontakt oss</a></li>
-                            </nav>
-                        </div>
-                    </div>
-                    <div class="content">
-
-                    </div>
                 </div>
-
             </div>
+            <div class="row">
+                <div class="col-10">
+                    <nav id="nav">
+                        <li class="current"><a href="../index.php">Hjem</a></li>
+                        <li><a href="#">Last opp bilde</a>
+                            <ul>
+                                <li><a href="#">Last opp et bilde</a>
+                                    <ul>
+                                        <li><a href="#">Samling</a></li>
+                                        <li><a href="#">Jørgen</a></li>
+                                        <li><a href="#">Erik</a></li>
+                                        <li><a href="#">Nok en under-element</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Last opp flere bilder</a>
+                                    <ul>
+                                        <li><a href="#">Dette er en my undermeny</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">IconDock</a></li>
+                                <li><a href="#">Beste Web BildeGalleri</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Historikk</a>
+                            <ul>
+                                <li><a href="#">Team</a>
+                                    <ul>
+                                        <li><a href="#">Sub-Level Item</a></li>
+                                        <li><a href="#">Sub-Level Item</a>
+                                            <ul>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                                <li><a href="#">Sub-Level Item</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Sub-Level Item</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Historikk bilder</a></li>
+                                <li><a href="#">Test statestikk</a></li>
+                                <li><a href="#">Popcorn</a>
+                                    <ul>
+                                        <li><a href="#">Sub-Level Item</a></li>
+                                        <li><a href="#">Sub-Level Item</a></li>
+                                        <li><a href="#">Sub-Level Item</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Om</a></li>
+                        <li><a href="#">Kontakt oss</a></li>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4" id="contentIntro"></div>
+                <div class="col-8" id="statisticContent"></div>
+            </div>
+            <div class="row">
 
+                <div class="col-4" id="contentGraph"></div>
+            </div>
         </body>
         <footer>
-
         </footer>
     </html>
