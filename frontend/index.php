7fd4c29ff895cde1f4d4059940f0ac1ac6903264
Erik Røed <post@erikroed.no>
2016-02-01 12:01:48 +0100

    Stylet checkboxer

--- frontend/index.php
+++ frontend/index.php
@@ -1262,24 +1262,7 @@
     </nav>
 
     <main class="Content">
-        <div id ="ContentText">
-            <h1 id="title">Velkommen til Vardens <br> digitale bildearkiv</h1>
-            <p id="introTxt">
-                Du kan nå søke i rundt 25 000 bilder via vårt bildearkiv på nett. <br>
-                Dette er over halvparten av Vardens fotosamling som består av totalt 90 000 fotografier.
-            </p>
-            <form method="post" name="searchfield" id="searchfield">
-                <input type="text" name="searchInput" id="searchInput" autofocus="autofocus" placeholder="Søk her..." required />
-                <input type="submit" name="search" id="search" src="img/searchbutton.svg" value=" "/>
-                <br>
-                <br>
-                <label><input type="checkbox" name="news" id="news"> Nyheter </label>
-                <input type="checkbox" name="culture" id="culture"> Kultur
-                <input type="checkbox" name="sport" id="sport"> Sport
-                <input type="checkbox" name="places" id="places"> Steder <br>
-            </form>
-            <h1 id="mostpictures">Mest viste bilder</h1>
-        </div>
+        <?php include('inc/hoved.php'); ?>
     </main>
 
     <footer class="Footer">
