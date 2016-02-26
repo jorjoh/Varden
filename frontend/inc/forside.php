f6189e5767c676c2a374e30fdbd5cffe132341a2
Erik Røed <post@erikroed.no>
2016-02-26 11:14:54 +0100

    Ordnet medie query på annonseplass og søkefeltet i frontend

--- frontend/inc/forside.php
+++ frontend/inc/forside.php
@@ -1,15 +1,15 @@
 <div class="row">
-    <h1 class="col-12" id="title">Velkommen til Vardens <br> digitale bildearkiv</h1>
+    <h1 id="title">Velkommen til Vardens <br> digitale bildearkiv</h1>
 </div>
 <div class="row">
-    <p class="col-12" id="introTxt">
+    <p id="introTxt">
         Du kan nå søke i rundt 25 000 bilder via vårt bildearkiv på nett. <br>
         Dette er over halvparten av Vardens fotosamling som består av totalt 90 000 fotografier. tes test test
     </p>
 </div>
 <?php include('searchfield.php'); ?>
 <div class="row">
-    <h1 class="col-12" id="mostpictures">Mest viste bilder</h1>
+    <h1 id="mostpictures">Mest viste bilder</h1>
     <img src="http://placehold.it/170x150">
     <img src="http://placehold.it/170x150">
     <img src="http://placehold.it/170x150">
@@ -19,11 +19,13 @@
 
 <br>
 
-<div id="abonnement">
-    <div id="text">
-        <h2 style="text-align: left;">Digital tilgang</h2>
-        <p>Få full tilgang til vårt bildearkiv ut året for kun 79kr,-
-            <a href="#" id="orderButton">Bestill her</a>
-        </p>
+<div class="row">
+    <div id="abonnement">
+        <div id="text">
+            <h2 style="text-align: left;">Digital tilgang</h2>
+            <p>Få full tilgang til vårt bildearkiv ut året for kun 79kr,-
+                <a href="#" id="orderButton">Bestill her</a>
+            </p>
+        </div>
     </div>
 </div>
