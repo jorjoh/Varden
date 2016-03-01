e8e4a27fd6441fd51b0feea50d4e7742546a1a27
Jørgen Johansen <post@jorgenjohansen.no>
2016-03-01 10:28:37 +0100

    integrert opplastnings-funksjonen

--- backend/inc/lastOppBilde.php
+++ backend/inc/lastOppBilde.php
@@ -1,63 +1,136 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jørgen Johansen
- * Date: 02.02.2016
- * Time: 08:51
- */
-include "/functions/datePicker.php";
-include "inc/uploads.php";
-?>
-<script type="text/javascript" src="script/backgroundResources/jquery.min.js"></script>
-<script type="text/javascript" src="script/backgroundResources/jquery.form.js"></script>
-<script type="text/javascript"src="script/uplodedImages.js"></script>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <!-- Add Dropzone -->
+    <link rel="stylesheet" type="text/css" href="style/dropzone.css"/>
+    <script type="text/javascript" src="script/dropzone.min.js"></script>
+    <script type="text/javascript" src="script/jquery.min.js"></script>
+</head>
 
-<div class="row" xmlns="http://www.w3.org/1999/html">
-    <div class="col-5">
-        <form method="post" name="multiple_upload_form" id="multiple_upload_form" enctype="multipart/form-data" action="inc/upload.php">
-            <input type="hidden" name="image_form_submit" value="1">
-            <label>Choose Image</label>
+<body>
 
-            <div class="image-upload">
-                <label for="images">
-                    <img src="http://goo.gl/pB9rpQ"/>
-                </label>
-                <input type="file" name="images[]" id="images" style="border: dashed 5px; 10px; height: 200px; width: 300px; text-align: center;" multiple>
-            </div>
+<p id="txt"></p>
 
-            <div class="uploading none">
-                <label>&nbsp;</label>
-                <img src="inc/uploading.gif" alt="uploading......"/>
-            </div>
-        </form>
+<h1>Drag&amp;Drop opplasting</h1>
+<form action="" method="post" class="dropzone" id="myForm" enctype="multipart/form-data">
+    <h4>Drag Files here to upload or <span class="btn btn-success fileinput-button dz-clickable"/> push this button!</h4>
+</form>
 
-    </div>
+    <div id="actions" class="row">
+    <!-- Knappene -->
+    <span class="btn btn-success fileinput-button dz-clickable"/>
+    <i class="glyphicon glyphicon-plus"></i>
+    <span>Legg til filer</span>
+    </span>
+
+    <button type="submit" id="upload" class="btn btn-primary start">
+        <i class="glyphicon glyphicon-upload"></i>
+        Last opp
+    </button>
+
+
+    <button data-dz-remove id="cancel" class="btn btn-warning cancel">
+        <i class="glyphicon glyphicon-ban-circle"></i>
+        Cancel</button>
 </div>
 
-<div class="row">
-    <div class="col-12">
-        <?php for($i=0; $i<5; $i++){
-          ?>  <table>
-            <tr>
-                <th>Bilde</th>
-                <th>Tittel</th>
-                <th>Fotograf</th>
-                <th>Dato</th>
-                <th>Sted</th>
-            </tr>
-            <tr>
-                <td><div id="images_preview" style="width: 30px; height: 30px;"></div></td>
-                <td>Fint bilde</td>
-                <td> Erik</td>
-                <td><?php getdateTime(); ?></td>
-                <td>Borre,Horten</td>
-            </tr>
-        </table> <?php
-        } ?>
 
+<!--Område som enkeltbilder vises-->
+<div class="table table-striped files" id="previews" >
 
+    <div id="template" class="file-row" style="border: solid 1px #f9f9f9; position: relative; top: 10px; background-color: #f9f9f9">
 
+        <!-- This is used as the file preview template -->
+        <!--Div-tag som styler hele thumbnail-preview visningen-->
+        <div style="position:relative; top: 15px; height: 130px ">
+            <div style=" width: 122px; position: relative; left: 90px;">
+            <span class="preview"><img data-dz-thumbnail/></span>
+        </div>
+            <!--div-tag som styler 'name' til filen som lastes opp-->
+        <div style="width: 300px; left: 400px; bottom: 122px; position: relative;">
+            <p class="name" data-dz-name></p>
+
+            <strong class="error text-danger" data-dz-errormessage></strong>
+        </div>
+
+            <!--Progressbar -->
+        <div style="width: 150px; left: 800px; bottom: 150px; position: relative;">
+            <p class="size" data-dz-size></p>
+
+            <!-- Selve progressbaren-->
+            <div class="progress">
+                <div class="progress-bar progress-bar-success" id="test" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" data-dz-uploadprogress>
+                </div>
+            </div>
+            <!--Knapp som sletter enkeltbilde i køen-->
+            <button data-dz-remove id="cancel2" class="btn btn-warning cancel" style=" position: relative;">
+                <i class="glyphicon glyphicon-ban-circle"></i>
+                Cancel</button>
+        </div>
+
+        </div>
     </div>
-    <input type="button" name="avbryt" id="avbryt" value="Avbryt" style="padding: 5px 5px; border-radius: 10px"/>
-    <input type="button" name="godkjenn" id="godkjenn" value="Godkjenn" style="padding: 5px 5px; border-radius: 10px"/>
+
 </div>
+
+<script>
+    // Deklarerer variabel som plukker opp div-taggen template som skal brukes i visning av opplastende filer
+    var previewNode = document.querySelector("div#template");
+    previewNode.id = "";
+
+    // Ting som jeg ikke helt forstår
+    var previewTemplate = previewNode.parentNode.innerHTML;
+    previewNode.parentNode.removeChild(previewNode);
+
+    //Sørger for at ikke queue'n blir kjørt automatisk før 'go'-knappen er trykket
+    Dropzone.autoDiscover = false;
+    //Sperre som gjør at opplastningsfunksjonen kun tar imot bilder og ikke dokumenter.. Btw .svg filer fungerer også
+    var acceptedFileTypes = "image/*";
+
+    //Deklarerer selve dropzonen og definerer noen variabler fra bibiloteket til dropzone
+    var myDropzone = new Dropzone(document.body, {
+        url: 'inc/uploads.php',
+        autoProcessQueue: false,
+        paramName: 'file',
+        maxFiles:10,
+        previewTemplate: previewTemplate,
+        previewsContainer: "#previews",
+        headers: {"MyAppname-Service-Type": "Dropzone"},
+        acceptedFiles: acceptedFileTypes,
+        clickable:".fileinput-button"
+
+    });
+
+    //Registrerer knappentrykk og kjører kode
+    $('#upload').click(function () {
+       //Prosesserer køen
+        myDropzone.processQueue();
+
+        //'Success'-event som kan høres på og kjøres kode etter alle filer er akseptert
+        myDropzone.on("success", function(file,responseText){
+            console.log(file);
+            var txt = document.getElementById("txt");
+            txt.innerHTML = responseText;
+            removeContentDelay();
+        });
+    });
+
+    //Avbyrt-knapp som sletter hele køen
+    $('#cancel').click(function () {
+        myDropzone.removeAllFiles();
+    });
+
+    // Funksjon som setter en forsinkelse på hendelsen removeAllFiles
+    function removeContentDelay() {
+        timeoutID = window.setTimeout(removeAllFilesAfterDelay,2000);
+    }
+    // Fuksjon som tømmer køen etter perdefinert tid i funksjonen removeContentDelay
+    function removeAllFilesAfterDelay(responseText){
+        var txt = document.getElementById("txt");
+        txt.innerHTML = ("<h3 style='position: relative;'>Successfully uploaded</h3>");
+
+        myDropzone.removeAllFiles();
+    }
+ </script>
+</body>
+</html>
