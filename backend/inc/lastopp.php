3bd14996115dac15036087ddc1df9b3cebd0b6f3
Erik Røed <post@erikroed.no>
2016-03-01 10:46:10 +0100

    Lagt til ny fil for last opp med array over gyldige filtyper

---
+++ backend/inc/lastopp.php
@@ -0,0 +1,11 @@
+<?php
+
+    $validImageType = array(
+        "IMAGETYPE_GIF",
+        "IMAGETYPE_JPEG",
+        "IMAGETYPE_PNG",
+        "IMAGETYPE_PSD",
+        "IMAGETYPE_BMP",
+    );
+
+?>
