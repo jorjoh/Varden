16632d110f2100cd5be7c008958c4c4a6f6e3d3b
Erik Røed <post@erikroed.no>
2016-03-24 23:41:42 +0100

    Fikset opplasting til mappe

---
+++ backend/bootstrap/inc/uploads.php
@@ -0,0 +1,23 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Jørgen Johansen
+     * Date: 09.02.2016
+     * Time: 12:24
+     */
+
+    $uploaddir = 'uploads/';
+    $uploadfile = $uploaddir . basename($_FILES['file']['name']);
+
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+        echo "File is valid, and was successfully uploaded.\n HEEEI";
+    } else {
+        echo "Possible file uploads attack!\n";
+    }
+
+    echo '<pre>';
+    echo 'Here is some more debugging info:';
+    print_r($_FILES);
+
+    echo "</pre>";
+?>
