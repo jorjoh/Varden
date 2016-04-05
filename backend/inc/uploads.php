486228d0ed729d270e3a14adb487ca77933cc34b
Jørgen Johansen <post@jorgenjohansen.no>
2016-04-05 22:52:27 +0200

    revert

--- backend/inc/uploads.php
+++ backend/inc/uploads.php
@@ -42,6 +42,58 @@ echo("Fotograf er: " . $photographer . "\n");
 echo("Beskrivelse er: " . $beskrivelse . "\n");
 echo("URL for bilde er: <a href = '$urlforimage'> Trykk her for å se bilde </a>");
 
+/*------informasjon som skal inni arrayer i databasen*/
+$insdatatocamera = array(
+    'cameramaker' => $camera['make'],
+    'cameramodel' => $camera['model'],
+);
+
+$insdatatocategory = array(
+    'name' => "Valgt kategori", // her kan vi har noen checkbokser vel?
+);
+
+$insdatatoimagedesgin = array(  //Motiv
+    'name' => "Bildets motiv",
+);
+
+$insDataToImages = array(
+    'filename' => $uploadfile,
+    'picturetext' => $beskrivelse,
+    'thumb_w' => "120",
+    'thumb_h' => "120",
+    'url' => $urlforimage
+);
+$insdatatometainfo = array(
+    "capturedate" => $exifexifinfo["DateTimeOriginal"],
+    "w_original" => $exifcomputed['COMPUTED']['Width'],
+	"h_original" => $exifcomputed['COMPUTED']['Height'],
+	"imagetype" => $exiffile['MimeType'],
+	"resolution" =>$exifcomputed['XResolution'],
+	"bit_dept" => "Null", // hmm denne veriden ser ikke ut til å være her
+	"uploaded" => "var date = new Date; getDate",
+	"exposure_time" => $exifexifinfo['ExposureTime'],
+	"focal_length" => $exifexifinfo['FocalLength'],
+	"white_balance" => $exifexifinfo['WhiteBalance'],
+	"orientation" => $exifcomputed['Orientation'],
+	"iso_speed" => $exifexifinfo['ISOSpeedRatings'],
+	"flash_state" => "True/false", //Akkurat det tror jeg ikke vi her
+	"tags" => "illustrasjonsbilde",
+);
+$insdatattophotographers = array(
+    "firstname" => $photographer,
+    "lastname" =>$photographer, //Her burde vi legge tilrette for etternavn
+);
+$insdatatophysicallocation = array(
+    "room" => "The archive",
+    "drawer" => "3",
+    "folder" => "34",
+    "physicallocationcol" => "Vardens arkiv",
+);
+
+for($i = 0; $i <count($insdatatometainfo); $i++){
+    echo $insdatatometainfo[$i];
+    echo "dette er en melding fra foor-lopen";
+}
 
 /*------------ Slutt på funkjsonen */
 
