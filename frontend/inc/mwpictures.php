0979c8a592661cd61d1ee0cb5fa4b87c6fb45c13
Erik Røed <post@erikroed.no>
2016-04-13 20:10:51 +0200

    Fikset mest viste bilder på frontend

--- frontend/inc/mwpictures.php
+++ frontend/inc/mwpictures.php
@@ -1,20 +1,15 @@
 <?php
-    $sql = "SELECT url FROM images;";
+    $sql = "SELECT url FROM images ORDER BY count DESC LIMIT 5;";
     $result = mysqli_query($connect, $sql) or die(mysqli_error($connect));
     $rows = mysqli_num_rows($result);
 
+    echo "<div class='row'>
+        <h1 id='mostpictures'>Mest viste bilder</h1>
+    ";
     for($i = 0; $i < $rows; $i++) {
         $url = mysqli_fetch_array($result);
         $row = $url['url'];
-        echo $row."<br>";
+        echo "<a href='$row'><img src='$row' style='height: 170px; width: 150px; margin-left: 5px;'></a>";
     }
+    echo "</div>";
 ?>
-
-<div class="row">
-    <h1 id="mostpictures">Mest viste bilder</h1>
-    <img src="http://placehold.it/170x150">
-    <img src="http://placehold.it/170x150">
-    <img src="http://placehold.it/170x150">
-    <img src="http://placehold.it/170x150">
-    <img src="http://placehold.it/170x150">
-</div>
