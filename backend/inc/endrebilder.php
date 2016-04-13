ed771bc3a1c00130dcb3cb1c784badda2d71da0b
Jørgen Johansen <post@jorgenjohansen.no>
2016-04-13 19:29:13 +0200

    fixes table 'forside'

--- backend/inc/endrebilder.php
+++ backend/inc/endrebilder.php
@@ -17,7 +17,7 @@ $antallRader = mysqli_num_rows($sqlresultat);
 echo("<table class=\"mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp\">");
 echo("<thead>");
 echo(" <tr>
-        <th class=\\'mdl - data - table__cell--non - numeric\\'>Bilder(filename)</th>
+        <th class=\\'mdl-data-table__cell--non-numeric\\'>Bilder(filename)</th>
         <th>Beskrivelse</th>
         <th>URL</th>
         <th style='text-align: right'>Antall ganger vist</th>
@@ -33,7 +33,7 @@ for ($r = 1; $r <= $antallRader; $r++) {
     </thead>
     <tbody>
     <tr>
-        <td class=\\'mdl - data - table__cell--non - numeric\\'>$filnavn</td>
+        <td class=\\'mdl-data-table__cell--non-numeric\\'>$filnavn</td>
         <td>$beskrivelse</td>
         <td><a href='$url' class='slideshow_zoom'>Link til bilde</a> </td>
         <td style='text-align: right'>$count</td>
@@ -51,17 +51,11 @@ echo("</table>");
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>jQuery UI Dialog - Modal message</title>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="//code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
 
-    <div class="dialogBox" style="border:1px solid gray;">
-        <a href="inc/DataGrip.JPG" class="slideshow_zoom">Test</a>
 
-        <!-- TODO: Change above href -->
-        <!-- NOTE: Must be a local url, not cross domain -->
-    </div>
 
     <script type="text/javascript">
         $('.slideshow_zoom').each(function() {
@@ -86,11 +80,7 @@ echo("</table>");
 
     </script>
 </head>
-<body>
 
-
-
-</body>
 </html>
 
 
