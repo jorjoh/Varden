9124b6a8db6297452e518c3667bc3bd5b07d8f44
Erik Røed <post@erikroed.no>
2016-04-16 19:43:23 +0200

    Fikset UTF-8 problem på bilde.php

--- frontend/index.php
+++ frontend/index.php
@@ -9,7 +9,7 @@
 
     <meta name="robots" content="noarchive">
 
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
