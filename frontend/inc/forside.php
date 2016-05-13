2b77544d4f35424b578ce0b5ceb0e09b9a98dd38
Jørgen Johansen <post@jorgenjohansen.no>
2016-05-13 14:03:15 +0200

    Update forside.php

--- frontend/inc/forside.php
+++ frontend/inc/forside.php
@@ -7,7 +7,7 @@
 <h1 class="Article-heading-1" style="color:#FF0000; padding-top: 150px; font-size: 40pt; font-family: 'FlamaFont Slab', 'Roboto Slab', georgia, serif;">Velkommen til Vardens <br> digitale bildearkiv</h1>
 <h1>Test heading AUTODEPLOY codeship</h1>
 <h1>Shipyardtestcontiniousdeploymenttestnewserver</h1>
-<h2>Test heading2</h2>
+<h2>Test2 heading2</h2>
 <br>
 <p class="Article-header-entering" style="font-family: FlamaFont, Roboto, helvetica, arial, sans-serif;">
     Du kan nå søke i <strong><?php echo $numberOfPictures ?></strong> bilder via vårt bildearkiv på nett. <br>
