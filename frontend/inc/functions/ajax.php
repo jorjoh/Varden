e396033c9dadbdd7764109b0154bf48df8e93681
Erik Røed <post@erikroed.no>
2016-05-16 00:35:12 +0200

    Fikset infinite scroll og ordnet kategoriseringen fra bildeinfo

--- frontend/inc/functions/ajax.php
+++ frontend/inc/functions/ajax.php
@@ -10,18 +10,17 @@ require_once('dbcon.php');
 //$per_page = intval($_POST['per_page']);
 $searchtxt = $_POST['searchtxt'];
 $load = htmlentities(strip_tags($_POST['load']));
-$sql = "SELECT id, thumb_url, thumb_w FROM images WHERE picturetext LIKE '%$searchtxt%' OR filename LIKE '%$searchtxt%' LIMIT $load, 50;";
+$sql = "SELECT images.id, images.thumb_url, images.thumb_w, category.name FROM images JOIN category ON images.id = category.id WHERE picturetext LIKE '%$searchtxt%' OR filename LIKE '%$searchtxt%' LIMIT $load, 50;";
 $result = mysqli_query($connect, $sql) or die('Her skjedde det en feil! '. mysqli_error($connect));
 
-$category = array("nyheter", "kultur", "sport", "steder");
 while($row = mysqli_fetch_array($result)) {
     $id = $row['id'];
+    $category = $row['name'];
     $url = $row['thumb_url'];
     $width = $row['thumb_w'];
-    $categoryID = rand(0, 3);
     ?>
     <a href="?side=bilde&id=<?php echo $id; ?>">
-        <div class="single_pictures <?php echo $category[$categoryID]; ?>">
+        <div class="single_pictures <?php echo $category; ?>">
             <img class="lazy" data-original="<?php echo $url; ?>" width="<?php echo $width; ?>" height="100">
         </div>
     </a>
--- frontend/inc/resultat.php
+++ frontend/inc/resultat.php
@@ -24,7 +24,7 @@
                 <?php
 
                     if(!empty($searchtxt)) {
-                        $sql = "SELECT id, thumb_url, thumb_w FROM images WHERE picturetext LIKE '%$searchtxt%' OR filename LIKE '%$searchtxt%' LIMIT $start_from, $per_page;";
+                        $sql = "SELECT images.id, images.thumb_url, images.thumb_w, category.name FROM images JOIN category ON images.id = category.id WHERE picturetext LIKE '%$searchtxt%' OR filename LIKE '%$searchtxt%' LIMIT $start_from, $per_page;";
                         $query = "SELECT count(id) AS nbr FROM images WHERE picturetext LIKE '%$searchtxt%' OR filename LIKE '%$searchtxt%';";
                         $result = mysqli_query($connect, $sql) or die("Kunne ikke sende spørring til DB ". mysqli_error($connect));
                         $nbrresult = mysqli_query($connect, $query) or die ('Kunne ikke telle antall treff'. mysqli_error($connect));
@@ -42,7 +42,6 @@
                                 her som vises uansett om du har adblock eller ikke
                             </div> -->
                             <br>
-                            <p>'.$start_from.'</p>
                             <p>Ditt søk etter "'.$searchtxt.'" ga '. $nbrofrows.' resultater </p><br>
                             <div class="category_filter text-uppercase">
                                 <ul>
@@ -59,16 +58,15 @@
                             <div class="category_items">
                                 <div class="grid-sizer"></div>
                             ';
-                            $category = array("nyheter", "kultur", "sport", "steder");
                             while($row = mysqli_fetch_array($result)) {
                                 $id = $row['id'];
+                                $category = $row['name'];
                                 $url = $row['thumb_url'];
                                 $width = $row['thumb_w'];
-                                $categoryID = rand(0, 3);
 
                                 echo "
                                 <a href='?side=bilde&id=$id'>
-                                    <div class='single_pictures $category[$categoryID]'>
+                                    <div class='single_pictures $category'>
                                         <img class='lazy' data-original='$url' width='$width' height='100'>
                                     </div>
                                 </a>
@@ -87,27 +85,29 @@
                                 var load = 50;
                                 var nbr = <?php echo $nbrofrows; ?>;
                                 var queryword = "<?php echo $searchtxt; ?>";
-                                if (load > nbr) {
-                                    $('.loader').hide();
-                                } else {
-                                    $('#btn').click(function () {
-                                        load += 50;
+
+                                $('#btn').click(function () {
+                                    if(load > nbr) {
+                                        $('.loader').hide();
+                                        $('.messages').text("Kunne ikke finne flere bilder å laste");
+                                    }
+                                    else {
                                         $.ajax({
-                                                method: 'POST',
-                                                url: 'inc/functions/ajax.php',
-                                                data: {
-                                                    load: load,
-                                                    searchtxt: queryword
-                                                },
-                                            })
+                                            method: 'POST',
+                                            url: 'inc/functions/ajax.php',
+                                            data: {
+                                                load: load,
+                                                searchtxt: queryword
+                                            },
+                                        })
                                             .done(function (data) {
                                                 $('.category_items').append(data);
                                                 $('.category_items').isotope('reloadItems').isotope({sortBy: 'original-order'});
                                                 $('img.lazy').lazyload();
                                             });
-                                    });
-                                }
-                                $('.test').text("load = " + load + " nbr = " + nbr);
+                                    }
+                                    load += 50;
+                                });
                             });
                         </script>
                         <?php
@@ -120,5 +120,5 @@
     <div class='loader'>
         <a id='btn' style='padding: 0 50px;'>Se flere</a>
     </div>
-    <p class="test">BYTT DA</p>
+    <p class="messages"></p>
 </div>
